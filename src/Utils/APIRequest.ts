7a1bc1851ebffa6021d6c50323ba50291e71e056
Rohan Kumar <rohan.shuvam@gmail.com>
2022-02-02 19:36:21 +0530

    Changed to class method for much easier access

--- src/Utils/APIRequest.ts
+++
@@ -1,7 +0,0 @@
-import fetch from "node-fetch";
-
-export async function makeRequest(urlString: string): Promise<any> {
-    return await fetch(`https://api.jikan.moe/v4/${urlString}`, {
-        method: "GET",
-    }).then((res) => res.json());
-}
